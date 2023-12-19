d0c64011e2c8304a601e4acc552873eaa02fe478
zohaibulhassan <zohaibulhassan67@gmail.com>
2023-12-20 02:02:18 +0500

    signalR Assement with References

---
+++ NotificationApp-JavaScript/SignalRWebpages/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebpages.Hubs
+{
+    public class ChatHub :Hub
+    {
+        public async Task SendMessage(String user, String message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+    }
+}
